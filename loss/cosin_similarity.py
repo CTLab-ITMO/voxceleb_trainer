7d9ae835167ccd074b35adb2e6b9efce094943ad
walerina <78067929+walerina@users.noreply.github.com>
2025-07-19 21:05:36 +0300

    Add cos similarity

---
+++ loss/cosin_similarity.py
@@ -0,0 +1,61 @@
+#! /usr/bin/python
+# -*- encoding: utf-8 -*-
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import time, pdb, numpy
+from utils import accuracy
+
+
+class LossFunction(nn.Module):
+    def __init__(self, **kwargs):
+        super(LossFunction, self).__init__()
+
+        print('Initialised Cosin Similarity Loss')
+
+    def forward(self, outp, label=None):
+        # Нормализация эмбеддингов
+        outp_norm = F.normalize(outp, p=2, dim=-1)
+
+        # 1. Внутригрупповая схожесть (должна → 1 для всех элементов, т.к. голоса внутри группы принадлежат одному спикеру)
+        intra_sim = torch.bmm(outp_norm, outp_norm.transpose(1, 2))
+        intra_target = torch.ones_like(intra_sim).cuda()
+        intra_loss = F.mse_loss(intra_sim, intra_target)
+
+        # 2. Межгрупповая схожесть (должна → 0, т.к. эмбеддинги из разных спикеров, 1 только по диагонали, т.к. по ней сравнение элемента с самим собой)
+        inter_sim = torch.mm(
+            outp_norm.mean(dim=1),  # Усредненные по спикеру (по группе голосов) эмбеддинги [batch_size, emb_dim]
+            outp_norm.mean(dim=1).t()
+        )
+        inter_target = torch.eye(inter_sim.size(0)).cuda()
+        inter_loss = F.mse_loss(inter_sim, inter_target)
+
+        # 3. Расчет accuracy
+        with torch.no_grad():
+            matrix_mask = torch.eye(inter_sim.size(0), device=inter_sim.device).bool() # Маска, чтобы исключить из подсчета точности сравнение с самим собой (диаг.)
+
+            # Для внутригрупповой accuracy
+            intra_pred = (intra_sim > 0.5).float() # Если больше 0.5, считаем, что элементы признаны равными
+            intra_correct = (intra_pred == intra_target).float()
+            intra_acc = (intra_correct[~matrix_mask].mean() * 100).item()
+
+            # Для межгрупповой accuracy
+            inter_pred = (inter_sim > 0.5).float()
+            inter_correct = (inter_pred == inter_target).float()
+            inter_acc = (inter_correct[~matrix_mask].mean() * 100).item()
+
+            # Общая accuracy (взвешенное среднее)
+            total_acc = (intra_acc * self.nPerSpeaker + inter_acc * (inter_sim.size(0) - 1)) / \
+                        (self.nPerSpeaker + inter_sim.size(0) - 1)
+
+            '''
+            Для intra_acc: каждый спикер вносит nPerSpeaker сравнений (все попарные комбинации его отрезков)
+            Для inter_acc: каждый спикер сравнивается с (batch_size - 1) другими спикерами (inter_sim.size(0) = batch_size)
+            Знаменатель - для нормализации
+            '''
+
+        # Комбинированный лосс
+        total_loss = intra_loss + inter_loss
+
+        return total_loss, total_acc
--- trainSpeakerNet.py
+++ trainSpeakerNet.py
@@ -36,7 +36,7 @@ parser.add_argument('--seed',           type=int,   default=10,     help='Seed f
 ## Training details
 parser.add_argument('--test_interval',  type=int,   default=10,     help='Test and save every [test_interval] epochs')
 parser.add_argument('--max_epoch',      type=int,   default=500,    help='Maximum number of epochs')
-parser.add_argument('--trainfunc',      type=str,   default="amsoftmax",     help='Loss function')
+parser.add_argument('--trainfunc',      type=str,   default="cosin_similarity",     help='Loss function')
 
 ## Optimizer
 parser.add_argument('--optimizer',      type=str,   default="adam", help='sgd or adam')
